afce291eb99f1b88a69621ecd698411bfa992f3e
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-13 18:05:44 -0400

    Created first Functional Component

--- src/components/app.js
+++ src/components/app.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import moment from "moment";
 
 
-import PortfolioContainer from "./portfolio-container"
+import PortfolioContainer from "./portfolio/portfolio-container"
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Chris Nickel React Portfolio</h1>
-        <PortfolioContainer />
           <div>
             {moment().format('MMMM Do YYYY, h:mm:ss a')}
           </div>
+          <PortfolioContainer />
       </div>
     );
   }
