ff7c17f53759c00db7ccf2c39dab5674013479f3
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-18 18:04:07 -0400

    axios install and Initial API GET Request in React

--- src/components/app.js
+++ src/components/app.js
@@ -1,34 +1,56 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import React, { Component } from "react";
 import moment from "moment";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
-import NavigationContainer from "./Navigation/navigation-container";
-import Home from "./Pages/home";
-import About from "./Pages/about";
-import Contact from "./Pages/contact";
-import Blog from "./Pages/blog";
-import PortfolioDetail from "./portfolio/portfolio-detail"
-import NoMatch from './Pages/no-match'
 export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <div>
-          <h1>Chris Nickel React Portfolio</h1>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-         </div>
+  constructor() {
+    super();
 
+    this.getPortfolioItems = this.getPortfolioItems.bind(this);
+  }
+
+  getPortfolioItems() {
+    axios
+      .get('https://chrisnickel.devcamp.space/portfolio/portfolio_items')
+      .then(response => {
+        console.log("response data", response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  render() {
+    this.getPortfolioItems();
+    return (
+      <div className="app">
         <Router>
-           <NavigationContainer />
-           <Switch>
-             <Route exact path="/" component={Home} />
-             <Route path="/About-me" component={About} />
-             <Route path="/Contact" component={Contact} />
-             <Route path="/Blog" component={Blog} />
-             <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
-             <Route component={NoMatch} />
-           </Switch>
+          <div>
+            <h1>Chris Nickel Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
         </Router>
       </div>
     );
