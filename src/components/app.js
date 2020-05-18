bba09e2506031f08989eea297d38c46f86553a73
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-18 15:30:10 -0400

    Route Setup in React

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import moment from "moment";
 
 
 import PortfolioContainer from "./portfolio/portfolio-container"
 import NavigationContainer from "./Navigation/navigation-container"
+import Home from "./Pages/home"
+import About from "./Pages/about"
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <NavigationContainer />
+        
+
+        <Router>
+          <div>
+           <NavigationContainer />
+
+           <Switch>
+             <Route exact path="/" component={Home} />
+             <Route path="/About-me" component={About} />
+           </Switch>
+          </div>
+        </Router>
+
         <h1>Chris Nickel React Portfolio</h1>
           <div>
             {moment().format('MMMM Do YYYY, h:mm:ss a')}
