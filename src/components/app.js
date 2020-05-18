cde85caee387ed0d421932223d1999091cff6af0
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-18 13:53:33 -0400

    Use Ternary Operators in JSX Code

--- src/components/app.js
+++ src/components/app.js
@@ -3,10 +3,12 @@ import moment from "moment";
 
 
 import PortfolioContainer from "./portfolio/portfolio-container"
+import NavigationContainer from "./Navigation/navigation-container"
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>Chris Nickel React Portfolio</h1>
           <div>
             {moment().format('MMMM Do YYYY, h:mm:ss a')}
