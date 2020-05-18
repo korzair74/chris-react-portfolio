7dadfde6914cda6c2bc32aae366a8668fb9fdd73
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-18 17:28:40 -0400

    Catch All Route with React Router

--- src/components/app.js
+++ src/components/app.js
@@ -9,6 +9,7 @@ import About from "./Pages/about";
 import Contact from "./Pages/contact";
 import Blog from "./Pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail"
+import NoMatch from './Pages/no-match'
 export default class App extends Component {
   render() {
     return (
@@ -25,7 +26,8 @@ export default class App extends Component {
              <Route path="/About-me" component={About} />
              <Route path="/Contact" component={Contact} />
              <Route path="/Blog" component={Blog} />
-             <Route path="/portfolio/:slug" component={PortfolioDetail} />
+             <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+             <Route component={NoMatch} />
            </Switch>
         </Router>
       </div>
--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 
 export default function(props) {
+  console.log(props)
   return (
     <div>
       <h1>Portfolio Detail for {props.match.params.slug}</h1>
