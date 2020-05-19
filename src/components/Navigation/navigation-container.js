d5969c0c21149bbaa55260d1ffed83f820a621f0
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-19 18:17:23 -0400

    started nav flexbox

--- src/components/Navigation/navigation-container.js
+++ src/components/Navigation/navigation-container.js
@@ -11,25 +11,30 @@ export default class NavigationComponent extends Component {
 
   render() {
     return (
-      <div>
-        <NavLink exact to="/" activeClassName="nav-link-active">
-          Home
-        </NavLink>
+      <div className="nav-wrapper">
+        <div className='left-side'>
+          <NavLink exact to="/" activeClassName="nav-link-active">
+            Home
+          </NavLink>
 
-        <NavLink to="/About-me" activeClassName="nav-link-active">
-          About
-        </NavLink>
+          <NavLink to="/About-me" activeClassName="nav-link-active">
+            About
+          </NavLink>
 
-        <NavLink to="/Contact" activeClassName="nav-link-active">
-          Contact
-        </NavLink>
+          <NavLink to="/Contact" activeClassName="nav-link-active">
+            Contact
+          </NavLink>
 
-        <NavLink to="/Blog" activeClassName="nav-link-active">
-          Blog
-        </NavLink>
-        
+          <NavLink to="/Blog" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
+          
 
-        {false ? <button>Blogg Add</button> : ''}
+          {false ? <button>Blogg Add</button> : ''}
+        </div>
+        <div className="right-side">
+          Chris Nickel
+        </div>
       </div>
     )
   };
