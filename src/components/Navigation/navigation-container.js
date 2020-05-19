48b7d5c89c1f66c17d394e872f15931a6192848b
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-19 18:26:41 -0400

    styled left side navigation

--- src/components/Navigation/navigation-container.js
+++ src/components/Navigation/navigation-container.js
@@ -13,25 +13,30 @@ export default class NavigationComponent extends Component {
     return (
       <div className="nav-wrapper">
         <div className='left-side'>
+        <div className='nav-link-wrapper'>
           <NavLink exact to="/" activeClassName="nav-link-active">
             Home
           </NavLink>
-
+        </div>  
+        <div className='nav-link-wrapper'>
           <NavLink to="/About-me" activeClassName="nav-link-active">
             About
           </NavLink>
-
+        </div> 
+        <div className='nav-link-wrapper'>
           <NavLink to="/Contact" activeClassName="nav-link-active">
             Contact
           </NavLink>
-
+        </div> 
+        <div className='nav-link-wrapper'>
           <NavLink to="/Blog" activeClassName="nav-link-active">
             Blog
           </NavLink>
-          
-
+        </div>     
+        <div className='nav-link-wrapper'>
           {false ? <button>Blogg Add</button> : ''}
         </div>
+      </div> 
         <div className="right-side">
           Chris Nickel
         </div>
