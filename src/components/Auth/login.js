c4f179f7ea9d31aa7881835515b9b3172dbc3290
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-20 18:49:06 -0400

    React's Form Submit Handler

--- src/components/Auth/login.js
+++ src/components/Auth/login.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			email: '',
+			password: '',
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	}
+
+	handleSubmit(event) {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	}
 	render() {
 		return (
 			<div>
 				<h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
-				<form>
-					<input type='text' />
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type='email'
+						name='email'
+						placeholder='Your Email'
+						value={this.state.email}
+						onChange={this.handleChange}
+					/>
 
-					<input type='password' />
+					<input
+						type='password'
+						name='password'
+						placeholder='Your Password'
+						value={this.state.password}
+						onChange={this.handleChange}
+					/>
 				</form>
-				<div>Button</div>
+				<div type='submit'>Login</div>
 			</div>
 		);
 	}
