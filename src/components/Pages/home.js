cf0a70e9551b948296f63e19aa19aaa463201f11
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-20 13:51:37 -0400

    Style Structure for the Homepage Portfolio Items

--- src/components/Pages/home.js
+++ src/components/Pages/home.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-
 import PortfolioContainer from '../portfolio/portfolio-container';
 
-
-export default function() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-        <PortfolioContainer />
-    </div>
-  )
+export default function () {
+	return (
+		<div>
+			<PortfolioContainer />
+		</div>
+	);
 }
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-
-export default function(props) {
-    const { id, description, thumb_image_url, logo } = props.item;
-  return (
-    <div>
-    <img src={thumb_image_url} />
-    <img src={logo} />
-      <div>{description}</div>
-    <Link to={`/portfolio/${id}`}>
-      Link
-    </Link>
-
-    </div>
-  )
+export default function (props) {
+	const { id, description, thumb_image_url, logo } = props.item;
+	return (
+		<div className='portfolio-item-wrapper'>
+			<img src={thumb_image_url} />
+			<img src={logo} />
+			<div>{description}</div>
+			<Link to={`/portfolio/${id}`}>Link</Link>
+		</div>
+	);
 }
