8d1b734a093b1d70d017bf6334823cea8212f3a1
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-20 18:21:32 -0400

    Creating the Initial Login Component

--- src/components/Pages/auth.js
+++ src/components/Pages/auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import loginImg from '../../../static/assets/images/auth/login.jpg';
+import Login from '../Auth/login';
 
 export default class Auth extends Component {
 	render() {
@@ -12,7 +13,7 @@ export default class Auth extends Component {
 					}}
 				/>
 				<div className='right-column'>
-					<h1>Login Component</h1>
+					<Login />
 				</div>
 			</div>
 		);
