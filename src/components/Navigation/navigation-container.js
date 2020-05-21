f55758984f12cc02277e746c6cc2a804b4442e0e
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-21 18:16:32 -0400

    refactored class to functional component

--- src/components/Navigation/navigation-container.js
+++ src/components/Navigation/navigation-container.js
@@ -1,46 +1,37 @@
-import React, { Component } from "react";
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
+const NavigationComponent = (props) => {
+	return (
+		<div className='nav-wrapper'>
+			<div className='left-side'>
+				<div className='nav-link-wrapper'>
+					<NavLink exact to='/' activeClassName='nav-link-active'>
+						Home
+					</NavLink>
+				</div>
+				<div className='nav-link-wrapper'>
+					<NavLink to='/About-me' activeClassName='nav-link-active'>
+						About
+					</NavLink>
+				</div>
+				<div className='nav-link-wrapper'>
+					<NavLink to='/Contact' activeClassName='nav-link-active'>
+						Contact
+					</NavLink>
+				</div>
+				<div className='nav-link-wrapper'>
+					<NavLink to='/Blog' activeClassName='nav-link-active'>
+						Blog
+					</NavLink>
+				</div>
+				<div className='nav-link-wrapper'>
+					{false ? <button>Blogg Add</button> : ''}
+				</div>
+			</div>
+			<div className='right-side'>Chris Nickel</div>
+		</div>
+	);
+};
 
-
-export default class NavigationComponent extends Component {
-  constructor() {
-    super();   
- }
-
-
-  render() {
-    return (
-      <div className="nav-wrapper">
-        <div className='left-side'>
-        <div className='nav-link-wrapper'>
-          <NavLink exact to="/" activeClassName="nav-link-active">
-            Home
-          </NavLink>
-        </div>  
-        <div className='nav-link-wrapper'>
-          <NavLink to="/About-me" activeClassName="nav-link-active">
-            About
-          </NavLink>
-        </div> 
-        <div className='nav-link-wrapper'>
-          <NavLink to="/Contact" activeClassName="nav-link-active">
-            Contact
-          </NavLink>
-        </div> 
-        <div className='nav-link-wrapper'>
-          <NavLink to="/Blog" activeClassName="nav-link-active">
-            Blog
-          </NavLink>
-        </div>     
-        <div className='nav-link-wrapper'>
-          {false ? <button>Blogg Add</button> : ''}
-        </div>
-      </div> 
-        <div className="right-side">
-          Chris Nickel
-        </div>
-      </div>
-    )
-  };
-}
+export default NavigationComponent;
