a1e514ad41d2c3d59eabad95536c18f09f2508c0
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-27 15:16:55 -0400

    Installing and Configuring Font Awesome in React

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PortfolioSidebarList = (props) => {
-	const portfolioLIst = props.data.map((portfolioItem) => {
-		return (
-			<div key={portfolioItem.id} className='portfolio-item-thumb'>
-				<div className='portfolio-item-thumb-img'>
-					<img src={portfolioItem.thumb_image_url} alt='' />
-				</div>
-				<h1 className='title'>{portfolioItem.name}</h1>
-				<h2>{portfolioItem.id}</h2>
-				<a onClick={() => props.handleDeleteClick(portfolioItem)}>
-					Delete
-				</a>
-			</div>
-		);
-	});
+  const portfolioLIst = props.data.map((portfolioItem) => {
+    return (
+      <div key={portfolioItem.id} className='portfolio-item-thumb'>
+        <div className='portfolio-item-thumb-img'>
+          <img src={portfolioItem.thumb_image_url} alt='' />
+        </div>
+        <h1 className='title'>{portfolioItem.name}</h1>
+        <h2>{portfolioItem.id}</h2>
+        <a onClick={() => props.handleDeleteClick(portfolioItem)}>
+          <FontAwesomeIcon icon='trash' />
+        </a>
+      </div>
+    );
+  });
 
-	return (
-		<div className='portfolio-sidebar-list-wrapper'>{portfolioLIst}</div>
-	);
+  return <div className='portfolio-sidebar-list-wrapper'>{portfolioLIst}</div>;
 };
 
 export default PortfolioSidebarList;
