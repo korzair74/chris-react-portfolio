adecdf4f7d0ab86695f2fbd0b81228ed46a4be1b
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-27 15:58:47 -0400

    Font Awesome Naming Requirements for React Components

--- src/components/Navigation/navigation-container.js
+++ src/components/Navigation/navigation-container.js
@@ -1,73 +1,76 @@
-import React from 'react';
-import { withRouter } from 'react-router';
-import { NavLink } from 'react-router-dom';
-import axios from 'axios';
+import React from "react";
+import { withRouter } from "react-router";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const NavigationComponent = (props) => {
-	const dynamicLink = (route, linkText) => {
-		return (
-			<div className='nav-link-wrapper'>
-				<NavLink to={route} activeClassName='nav-link-active'>
-					{linkText}
-				</NavLink>
-			</div>
-		);
-	};
+  const dynamicLink = (route, linkText) => {
+    return (
+      <div className='nav-link-wrapper'>
+        <NavLink to={route} activeClassName='nav-link-active'>
+          {linkText}
+        </NavLink>
+      </div>
+    );
+  };
 
-	const handleSignOut = () => {
-		axios
-			.delete('https://api.devcamp.space/logout', {
-				withCredentials: true,
-			})
-			.then((response) => {
-				if (response.status === 200) {
-					props.history.push('/');
-					props.handleSuccessfulLogout();
-				}
-				return response.data;
-			})
-			.catch((error) => {
-				console.log('Error signing out', error);
-			});
-	};
+  const handleSignOut = () => {
+    axios
+      .delete("https://api.devcamp.space/logout", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          props.history.push("/");
+          props.handleSuccessfulLogout();
+        }
+        return response.data;
+      })
+      .catch((error) => {
+        console.log("Error signing out", error);
+      });
+  };
 
-	return (
-		<div className='nav-wrapper'>
-			<div className='left-side'>
-				<div className='nav-link-wrapper'>
-					<NavLink exact to='/' activeClassName='nav-link-active'>
-						Home
-					</NavLink>
-				</div>
-				<div className='nav-link-wrapper'>
-					<NavLink to='/About-me' activeClassName='nav-link-active'>
-						About
-					</NavLink>
-				</div>
-				<div className='nav-link-wrapper'>
-					<NavLink to='/Contact' activeClassName='nav-link-active'>
-						Contact
-					</NavLink>
-				</div>
-				<div className='nav-link-wrapper'>
-					<NavLink exact to='/blog' activeClassName='nav-link-active'>
-						Blog
-					</NavLink>
-				</div>
-				<div>
-					{props.loggedInStatus === 'LOGGED_IN'
-						? dynamicLink('/portfolio-manager', 'Portfolio Manager')
-						: null}
-				</div>
-			</div>
-			<div className='right-side'>
-				Chris Nickel
-				{props.loggedInStatus === 'LOGGED_IN' ? (
-					<a onClick={handleSignOut}>Sign Out</a>
-				) : null}
-			</div>
-		</div>
-	);
+  return (
+    <div className='nav-wrapper'>
+      <div className='left-side'>
+        <div className='nav-link-wrapper'>
+          <NavLink exact to='/' activeClassName='nav-link-active'>
+            Home
+          </NavLink>
+        </div>
+        <div className='nav-link-wrapper'>
+          <NavLink to='/About-me' activeClassName='nav-link-active'>
+            About
+          </NavLink>
+        </div>
+        <div className='nav-link-wrapper'>
+          <NavLink to='/Contact' activeClassName='nav-link-active'>
+            Contact
+          </NavLink>
+        </div>
+        <div className='nav-link-wrapper'>
+          <NavLink exact to='/blog' activeClassName='nav-link-active'>
+            Blog
+          </NavLink>
+        </div>
+        <div>
+          {props.loggedInStatus === "LOGGED_IN"
+            ? dynamicLink("/portfolio-manager", "Portfolio Manager")
+            : null}
+        </div>
+      </div>
+      <div className='right-side'>
+        Chris Nickel
+        {props.loggedInStatus === "LOGGED_IN" ? (
+          <a onClick={handleSignOut}>
+            <FontAwesomeIcon icon='sign-out-alt' />
+          </a>
+        ) : null}
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(NavigationComponent);
