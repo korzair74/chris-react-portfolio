143060fa72dfa0a702b4941f23b1a4b360311b58
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-30 11:52:16 -0400

    Rendering the Blog Details to the Screen

--- src/components/Pages/blog-detail.js
+++ src/components/Pages/blog-detail.js
@@ -17,7 +17,9 @@ export default class BlogDetail extends Component {
         `https://chrisnickel.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
       )
       .then((response) => {
-        console.log("response", response);
+        this.setState({
+          blogItem: response.data.portfolio_blog,
+        });
       })
       .catch((error) => {
         console.log("getBlogItem error", error);
@@ -29,9 +31,18 @@ export default class BlogDetail extends Component {
   }
 
   render() {
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status,
+    } = this.state.blogItem;
+
     return (
       <div>
-        <h1>Blog detail</h1>
+        <h1>{title}</h1>
+        <img src={featured_image_url} alt='' />
+        <div>{content}</div>
       </div>
     );
   }
