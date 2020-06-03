f8ec70286e28371b5f506f160ca2afd7a427ff0e
Chris Nickel <chris.nickel.dev@gmail.com>
2020-06-02 23:22:54 -0400

    Initial Build Out of the Portfolio Detail Component

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
+import Axios from "axios";
 
-export default function (props) {
-	return (
-		<div>
-			<h1>Portfolio Detail for {props.match.params.slug}</h1>
-		</div>
-	);
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getPortfolioItem() {
+    Axios.get(
+      `https://chrisnickel.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
+      { withCredentials: true }
+    )
+      .then((response) => {
+        console.log("response", response);
+      })
+      .catch((error) => {
+        console.log("getPortfolioItem error", error);
+      });
+  }
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Portfolio Detail for {this.props.match.params.slug}</h1>
+      </div>
+    );
+  }
 }
